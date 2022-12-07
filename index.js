3b1d6560e435cbf096fd0f69a5275777ae2ab08a
samky444 <104277593+samky444@users.noreply.github.com>
2022-12-07 15:02:51 -0800

    Add files via upload
    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// When clicked, render a paragraph under the button(in the container)
+// that says "Thank you for buying"
+
+
+const container = document.getElementById("container")
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+function buy(){
+  container.innerHTML += "<p>Than you for buying!</p>"
+}
